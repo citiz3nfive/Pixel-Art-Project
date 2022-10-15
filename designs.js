adf865ab117fa092a3f55a567793d75f12bbdf22
citiz3nfive <melino_13@hotmail.com>
2022-10-15 11:07:44 +1100

    updates to code

--- designs.js
+++ designs.js
@@ -1,10 +1,19 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
-}
+$(document).ready(function() {});
+$('#sizePicker').submit(function makeGrid(grid) {
+    $('table tr').remove();
+    const h = $('#inputHeight').val();
+    const w = $('#inputWidth').val();
+    for (var i = 1; i <= h; i++) {
+        $('table').append('<tr></tr>');
+        for (var x = 1; x <= w; x++) {
+            $('tr:last').append('<td></td>');
+            $('td').attr('class', 'cell');
+        }
+    }
+    grid.preventDefault();
+    function makeGrid() {} 
+    $('.cell').click(function(event) {
+        var color = $('#colorPicker').val();
+        $(this).css('background-color', color);
+    });
+});
