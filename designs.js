b52f6660e5045b1d37789578fb3270b3cdc6e1de
citiz3nfive <melino_13@hotmail.com>
2022-10-15 11:15:03 +1100

    updated js

--- designs.js
+++ designs.js
@@ -6,8 +6,8 @@ $('#sizePicker').submit(function makeGrid(grid) {
     for (var i = 1; i <= h; i++) {
         $('table').append('<tr></tr>');
         for (var x = 1; x <= w; x++) {
-            $('tr:last').append('<td></td>');
             $('td').attr('class', 'cell');
+            $('tr:last').append('<td></td>');
         }
     }
     grid.preventDefault();
