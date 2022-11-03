9c8b2af080021b71da8e10c7bea8588ff93807ad
citiz3nfive <melino_13@hotmail.com>
2022-11-03 17:55:45 +1100

    <adding mentor notes and fixes to js and html>

--- designs.js
+++ designs.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {});
+/**
+* @description Creating a grid that will allow you to fill the squares up with different colors.
+* @param int $inputWidth - This will be the number of squares representing the width of your grid.
+* @param int $inputHeight - And this will represent the number of squares for the height of your grid.
+*/
 $('#sizePicker').submit(function makeGrid(grid) {
     $('table tr').remove();
-    const h = $('#inputHeight').val(); // Select size input
+    const h = $('#inputHeight').val();
     const w = $('#inputWidth').val();
     for (var i = 1; i <= h; i++) {
         $('table').append('<tr></tr>');
